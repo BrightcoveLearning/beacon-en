d75428884644ae054cc1d64f9963b154b68c68e3
Robert Crooks <rcrooks@brightcove.com>
2020-11-24 12:51:03 -0500

    cleanup

--- assets/js/site/site.js
+++ assets/js/site/site.js
@@ -30,45 +30,6 @@ var BCLS_site = (function(window, document) {
     back_button = document.getElementById('back_button');
 
 
-  // if inside iframe, hide appropriate elements
-  // if (window.location !== window.parent.location) {
-  //   var fby = fby || [];
-  //   fby.push(['showTab', {id: '{{ site.feedbackify_id }}', position: 'right', color: 'rgb(4, 13, 40)'}]);
-  //   (function () {
-  //       var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
-  //       f.src = '//cdn.feedbackify.com/f.js';
-  //       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
-  //   })();
-  //   hideElement(header);
-  //   hideElement(footer);
-  //   hideElement(side_nav);
-  //   hideElement(talla_wrapper);
-  //   hideElement(bc_veggie_burger_wrapper);
-    // in_prod_nav.removeAttribute('style');
-    // open_new_tab.removeAttribute('style');
-    // open_new_tab.setAttribute('href', window.location.href);
-    // removeFeedbackify();
-    // forward_button.addEventListener('click', function() {
-    //   window.history.forward();
-    // });
-    // back_button.addEventListener('click', function() {
-    //   window.history.back();
-    // });
-  }
-  function removeFeedbackify() {
-    var t,
-    feedbackify = document.getElementById('feedbackify');
-    console.log('feedbackify', feedbackify);
-    if (feedbackify) {
-      var feedbackify_parent = feedbackify.parentNode;
-      console.log('feedbackify_parent', feedbackify_parent);
-      feedbackify_parent.removeChild(feedbackify);
-    } else {
-      console.log('retry');
-      t = window.setTimeout(removeFeedbackify, 2000);
-    }
-
-}
 
 // utilities for adding/removing classes
 function hasClass(el, name) {
