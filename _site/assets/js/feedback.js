531b4a13a3c99bdb2c75a361b233e2c0fae5a589
Robert Crooks <rcrooks@brightcove.com>
2020-11-28 15:39:23 -0500

    wip

--- _site/assets/js/feedback.js
+++ _site/assets/js/feedback.js
@@ -1,5 +1,5 @@
 var fby = fby || [];
-fby.push(['showTab', {id: '15610', position: 'right', color: 'rgb(4, 13, 40)'}]);
+fby.push(['showTab', {id: '15610', position: 'right', color: 'rgb(9, 7, 140)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -3,7 +3,7 @@ layout: null
 ---
 
 var fby = fby || [];
-fby.push(['showTab', {id: '{{ site.feedbackify_id }}', position: 'right', color: 'rgb(4, 13, 40)'}]);
+fby.push(['showTab', {id: '{{ site.feedbackify_id }}', position: 'right', color: 'rgb(9, 7, 140)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
