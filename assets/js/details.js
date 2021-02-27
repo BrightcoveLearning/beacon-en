e2d7b686e10c874b1c9c83a19f19edbc66a56bd9
Robert Crooks <rcrooks@brightcove.com>
2021-02-27 11:00:25 -0500

    https://brightcove.atlassian.net/browse/KNOW-20344

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,11 +1,13 @@
 var BCLS_details = ( function (window, document) {
   var item_details = document.querySelectorAll('details.details-item'),
     section_details = document.querySelectorAll('details.details-section'),
+		nested_details = document.querySelectorAll('details.details-nested'),
     i,
     iMax = item_details.length,
     s,
-    sMax = section_details.length;
-console.log(section_details);
+    sMax = section_details.length,
+		n,
+		nMax = nested_details.length;
   function itemDetailHandler(evt) {
     var j;
     
@@ -13,6 +15,18 @@ console.log(section_details);
     evt.preventDefault();
     for (j = 0; j < iMax; j++) {
       item_details[j].removeAttribute('open');
+    }
+    this.setAttribute('open', 'true');
+    }
+  }
+  
+  function nestedDetailHandler(evt) {
+    var j;
+    
+    if (!this.hasAttribute('open')) {
+    evt.preventDefault();
+    for (j = 0; j < nMax; j++) {
+      nested_details[j].removeAttribute('open');
     }
     this.setAttribute('open', 'true');
     }
@@ -33,9 +47,15 @@ console.log(section_details);
   for (i = 0; i < iMax; i++) {
     item_details[i].addEventListener('click', itemDetailHandler);
   }
+
   if (sMax > 0) {
     for (s = 0; s < sMax; s++) {
       section_details[s].addEventListener('click', sectionDetailHandler);
+    }
+  }
+  if (nMax > 0) {
+    for (n = 0; n < nMax; s++) {
+      nested_details[n].addEventListener('click', nestedDetailHandler);
     }
   }
 })(window, document);
