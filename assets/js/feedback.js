97c89121173a9b6fe1c301d6598c76d5f913de54
Robert Crooks <rcrooks@brightcove.com>
2021-03-09 21:32:33 -0500

    general-update

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -3,7 +3,7 @@ layout: null
 ---
 
 var fby = fby || [];
-fby.push(['showTab', {id: '{{ site.feedbackify_id }}', position: 'right', color: 'rgb(9, 7, 140)'}]);
+fby.push(['showTab', {id: '{{ site.feedbackify_id }}', position: 'right', color: 'rgb(211, 61, 0)'}]);
 (function () {
     var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
     f.src = '//cdn.feedbackify.com/f.js';
