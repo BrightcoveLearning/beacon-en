fca3b174e898fcc8cd063ae0a4e9b94810c6c332
Bob Bailey <bbailey@brightcove.com>
2022-05-02 15:41:52 -0400

    $COMMITMESSAGE

--- install-submodules.sh
+++ install-submodules.sh
@@ -7,6 +7,7 @@ git submodule add https://github.com/BrightcoveLearning/s-players
 git submodule add https://github.com/BrightcoveLearning/s-layout
 git submodule add https://github.com/BrightcoveLearning/s-publish
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps
+git submodule add https://github.com/BrightcoveLearning/s-media
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
@@ -17,5 +18,6 @@ git submodule add https://github.com/BrightcoveLearning/s-players-img
 git submodule add https://github.com/BrightcoveLearning/s-layout-img
 git submodule add https://github.com/BrightcoveLearning/s-publish-img
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps-img
+git submodule add https://github.com/BrightcoveLearning/s-media-img
 cd ..
 git submodule update --init --recursive
