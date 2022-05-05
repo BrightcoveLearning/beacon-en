33c96eca41aa8e5bcccf71e9d710e597fffe875e
Matthew Boles <mboles@brightcove.com>
2022-05-05 09:52:07 -0400

    added general submodules

--- install-submodules.sh
+++ install-submodules.sh
@@ -10,6 +10,7 @@ git submodule add https://github.com/BrightcoveLearning/s-publish
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps
 git submodule add https://github.com/BrightcoveLearning/s-media
 git submodule add https://github.com/BrightcoveLearning/s-analytics
+git submodule add https://github.com/BrightcoveLearning/s-general
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
@@ -22,5 +23,6 @@ git submodule add https://github.com/BrightcoveLearning/s-publish-img
 git submodule add https://github.com/BrightcoveLearning/s-publishing-apps-img
 git submodule add https://github.com/BrightcoveLearning/s-media-img
 git submodule add https://github.com/BrightcoveLearning/s-analytics-img
+git submodule add https://github.com/BrightcoveLearning/s-general-img
 cd ..
 git submodule update --init --recursive
