53ab9159d62495281725c02afbf279a725c05d80
Matthew Boles <mboles@brightcove.com>
2022-05-31 10:41:50 -0400

    Added monetization submodules

--- install-submodules.sh
+++ install-submodules.sh
@@ -14,6 +14,7 @@ git submodule add https://github.com/BrightcoveLearning/s-analytics
 git submodule add https://github.com/BrightcoveLearning/s-general
 git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-campaign
+git submodule add https://github.com/BrightcoveLearning/s-monetization
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-ott-plugins-img
@@ -30,6 +31,7 @@ git submodule add https://github.com/BrightcoveLearning/s-general-img
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
 git submodule add https://github.com/BrightcoveLearning/s-campaign-img
+git submodule add https://github.com/BrightcoveLearning/s-monetization-img
 cd ..
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
